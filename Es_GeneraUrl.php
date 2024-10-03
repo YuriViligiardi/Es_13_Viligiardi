93cec9ef48c2b1eb0afdcf5fc053c9a6f6795264
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-03 10:30:05 +0200

    Aggiunta funzione generaUrlImmagine

--- Es_GeneraUrl.php
+++ Es_GeneraUrl.php
@@ -7,6 +7,14 @@
 </head>
 <body>
     <?php
+        $urlImmagine = generaUrlImmagine(rand(500,800), rand(300,400));
+        echo "<img src='$urlImmagine' alt='Immagine generata casualmente'>";
+
+
+
+        function generaUrlImmagine($l, $a){
+            return "https://picsum.photos/" . $l . "/" . $a;
+        }
     ?>
 </body>
 </html>
